e0631880debdd05555504098d90889a4e6892497
Enrique Sosa <elasosajr@gmail.com>
2023-10-23 11:51:39 -0700

    added more stuff

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log('Hi there!');
+const fetchData = async (search) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'd88f02e0',
+            s: search
+        }
+    });
+    
+    if(response.data.Error) {
+        return [];
+    };
+
+    return response.data.Search;
+};
+
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class="input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
+
+const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
+const onInput = async event => {
+    const movies = await fetchData(event.target.value);
+
+    for(let movie of movies) {
+        const div = document.createElement('div');
+
+        div.innerHTML = `
+            <img src="${movie.Poster}" />
+            <h1>${movie.Title}</h1>
+        `;
+
+        const target = document.querySelector('#target').appendChild(div);
+    };
+};
+
+input.addEventListener('input', debounce(onInput, 500));
